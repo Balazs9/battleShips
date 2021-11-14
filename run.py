26c490052ac35483c45f893b94325cce862b95f9
Balazs9 <sempergerbalazs@gmail.com>
2021-11-14 16:44:33 +0000

    Fixing typing error and add new_game function

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ from termcolor import colored
 
 scores = {"computer": 0, "player": 0}
 
+
 class Game:
     def __init__(self, size, number_ships, name, type):
         self.size = size
@@ -16,13 +17,36 @@ class Game:
     def print(self):
         for row in self.board:
             print(" ".join(row))
-    
+
     def guess(self, x, y):
         self.guesses.append((x, y))
         self.board[x][y] = "X"
 
         if (x, y) in self.ships:
-            slef.board[x] [y] = "*"
+            self.board[x][y] = "*"
             return "Hit"
         else:
             return "Missed"
+
+
+def new_game():
+    
+    """
+    Start the new game, sets ships numbers, board size, restart the scoretbale
+    """
+
+    size = 9
+    number_ships = 5
+    scores["computer"] = 0
+    scores["player"] = 0
+    print(colored("         *", "yellow"))
+    print(colored("     ******   ", "yellow"))
+    print(colored("     *    *   ", "yellow"))
+    print(colored("*    *    *  *", "yellow"))
+    print(colored(" ************", "yellow"))
+    print(colored("  **********", "yellow"))
+    print(colored("*" * 35, "red"))
+    player_name = input("Please enter your name: ")
+
+
+new_game()
