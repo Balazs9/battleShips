1f68bda7c7271bcb32d6f506693ed3a4c1ca8cf7
Balazs9 <sempergerbalazs@gmail.com>
2021-11-14 16:13:33 +0000

    Add class for game, function for print the board

--- run.py
+++ run.py
@@ -1,3 +1,28 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+from termcolor import colored
+
+scores = {"computer": 0, "player": 0}
+
+class Game:
+    def __init__(self, size, number_ships, name, type):
+        self.size = size
+        self.board = [["." for x in range(size)] for y in range(size)]
+        self.number_ships = number_ships
+        self.name = name
+        self.type = type
+        self.guesses = []
+        self.ships = []
+
+    def print(self):
+        for row in self.board:
+            print(" ".join(row))
+    
+    def guess(self, x, y):
+        self.guesses.append((x, y))
+        self.board[x][y] = "X"
+
+        if (x, y) in self.ships:
+            slef.board[x] [y] = "*"
+            return "Hit"
+        else:
+            return "Missed"
